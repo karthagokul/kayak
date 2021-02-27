c555b5f56c64b27947ea84d2898754db48a55567
Your Name <kartha.gokul@gmail.com>
2021-02-27 18:15:56 +0100

    code cleanup

---
+++ platform/engine/engine.cpp
@@ -0,0 +1,19 @@
+/*
+ *******************************************************************
+ * Copyright (C) 2021-2022 Bolgatty BV
+ * This file is part of Kayak.
+ * Kayak can not be copied and/or distributed without the express
+ * permission of Bolgatty BV
+ *******************************************************************
+*/
+
+#include "engine.h"
+namespace Kayak {
+
+Engine::Engine()
+{
+
+}
+
+}
+
---
+++ platform/engine/engine.h
@@ -0,0 +1,21 @@
+/*
+ *******************************************************************
+ * Copyright (C) 2021-2022 Bolgatty BV
+ * This file is part of Kayak.
+ * Kayak can not be copied and/or distributed without the express
+ * permission of Bolgatty BV
+ *******************************************************************
+*/
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+namespace Kayak {
+    class Engine
+    {
+    public:
+        Engine();
+    };
+}
+
+#endif // ENGINE_H
